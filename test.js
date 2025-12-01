9c3a9598b9ba80fc1df1a8d4b57a41a277f1a587
prifrali <131600845+prifrali@users.noreply.github.com>
2025-12-01 12:26:42 +0300

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger');
-const navMenu = document.getElementById('nav-menu');
-
-burger.addEventListener('click', function () {
-    navMenu.classList.toggle('nav--open');
-});
