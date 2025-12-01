ce252d41d00e442aa497aec7413b8343b6e40759
prifrali <131600845+prifrali@users.noreply.github.com>
2025-12-01 12:28:06 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById('burger');
+const navMenu = document.getElementById('nav-menu');
+
+burger.addEventListener('click', function () {
+    navMenu.classList.toggle('nav--open');
+});
